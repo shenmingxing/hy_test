1838576d926e3486de67cf2397a5c0e852514c67
smx <1547022735@qq.com>
2024-11-27 20:16:03 +0800

    增加容器编译

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module hy_test
 
 go 1.23.0
+
+require github.com/ahmetb/go-linq v3.0.0+incompatible
--- main.go
+++ main.go
@@ -1,7 +1,15 @@
 package main
 
-import "fmt"
+import (
+	"crypto/md5"
+	"fmt"
+	"github.com/ahmetb/go-linq"
+)
 
 func main() {
 	fmt.Println("Hello, wold")
+	fmt.Println(md5.Sum([]byte("hello")))
+	linq.From([]int{1, 2, 3}).ForEach(func(i interface{}) {
+		fmt.Println(i)
+	})
 }
