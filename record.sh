b998d3836c2d2741c812059ca11deb84a31895ca
smx <1547022735@qq.com>
2024-11-29 09:22:02 +0800

    file commd

--- record.sh
+++ record.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
-git pull
-date_str=$(date '+%Y-%m-%d %H:%M')
-echo "${date_str}: Record it" > ./README.md
 
-git add ./README.md
-git commit -m "${date_str}"
+echo "============>"
+#git pull
+#date_str=$(date '+%Y-%m-%d %H:%M')
+#echo "${date_str}: Record it" > ./README.md
+#
+#git add ./README.md
+#git commit -m "${date_str}"
